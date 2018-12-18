5abfdb9b5bd79083028a48b741bf05ab8c1f6636
Vladimir Dralo <vladra@users.noreply.github.com>
2018-12-18 17:17:05 +0200

    Reproduce issue

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import Vue from 'vue';
+import VueCustomElement from 'vue-custom-element';
+
 import App from './App.vue';
 
 Vue.config.productionTip = false;
 
-new Vue({
-  render: (h) => h(App),
-}).$mount('#app');
+Vue.use(VueCustomElement);
+
+Vue.customElement('my-widget', App);
